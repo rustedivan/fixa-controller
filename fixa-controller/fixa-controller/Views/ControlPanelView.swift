cc6cc3c6990da25bb9a5feabd8cad706ad62be22
Ivan Milles <ivan@rusted.se>
2023-05-07 11:47:11 +0200

    Setup control grouping using GroupBox instead of OutlineView

--- fixa-controller/fixa-controller/Views/ControlPanelView.swift
+++ fixa-controller/fixa-controller/Views/ControlPanelView.swift
@@ -36,28 +36,7 @@ struct ControlPanelView: View {
 				ActivityIndicator()
 			} else if clientState.connected {
 				ForEach(orderedControls, id: \.self.key) { (key, value) in
-					switch value {
-						case .bool(let display):
-							FixableToggle(value: self.clientState.fixableBoolBinding(for: key),
-														label: display.label)
-								.padding(.bottom)
-								.frame(maxWidth: .infinity)
-						case .float(let min, let max, let display):
-							FixableSlider(value: self.clientState.fixableFloatBinding(for: key),
-														label: display.label, min: min, max: max)
-								.padding(.bottom)
-								.frame(maxWidth: .infinity)
-						case .color(let display):
-							FixableColorWell(value: self.clientState.fixableColorBinding(for: key),
-															 label: display.label)
-								.padding(.bottom)
-								.frame(maxWidth: .infinity)
-						case .divider(let display):
-							Text(display.label)
-								.font(.headline)
-								.padding(.bottom)
-								.frame(maxWidth: .infinity)
-					}
+					insertGrouping(key: key, config: value)
 				}
 			}
 			Spacer()
@@ -72,6 +51,81 @@ struct ControlPanelView: View {
 		}.padding(16.0)
 		 .frame(minWidth: 320.0)
 	}
+	
+	@ViewBuilder
+	func insertGrouping(key: FixableId, config: FixableConfig) -> some View {
+		switch config {
+			case .divider(let display):
+				Text(display.label)
+					.font(.headline)
+					.padding(.bottom)
+					.frame(maxWidth: .infinity)
+			case .group(let contents, let display):
+				GroupBox(label: Text(display.label)) {
+					ForEach(contents, id: \.self.0.hashValue) { (key, value) in
+						insertControl(key: key, config: value)
+					}
+				}.padding(.bottom)
+			default:
+				insertControl(key: key, config: config)
+		}
+	}
+	
+	@ViewBuilder
+	func insertControl(key: FixableId, config: FixableConfig) -> some View {
+		switch config {
+			case .bool(let display):
+				FixableToggle(value: self.clientState.fixableBoolBinding(for: key),
+											label: display.label)
+									.padding(.bottom)
+									.frame(maxWidth: .infinity)
+			case .float(let min, let max, let display):
+				FixableSlider(value: self.clientState.fixableFloatBinding(for: key),
+											label: display.label, min: min, max: max)
+					.padding(.bottom)
+					.frame(maxWidth: .infinity)
+			case .color(let display):
+				FixableColorWell(value: self.clientState.fixableColorBinding(for: key),
+												 label: display.label)
+					.padding(.bottom)
+					.frame(maxWidth: .infinity)
+			default:
+				Text("Unmapped control: \(key.debugDescription)").font(.callout).foregroundColor(.red)
+		}
+	}
+}
+
+extension FixableConfig: Hashable {
+	public static func == (lhs: FixableConfig, rhs: FixableConfig) -> Bool {
+		return lhs.hashValue == rhs.hashValue
+	}
+	
+	public func hash(into hasher: inout Hasher) {
+		let displayHash: FixableDisplay
+		switch self {
+			case .bool(let display):
+				displayHash = display
+			case .float(_, _, let display):
+				displayHash = display
+			case .color(let display):
+				displayHash = display
+			case .divider(let display):
+				displayHash = display
+			case .group(_, let display):
+				displayHash = display
+		}
+		
+		hasher.combine(displayHash.label)
+		hasher.combine(displayHash.order)
+	}
+	
+	var childControls: [FixableConfig]? {
+		if case let .group(contents, _) = self {
+			return contents.map { $0.1 }
+		} else {
+			return nil
+		}
+	}
 }
 
 struct ControlPanelView_Previews: PreviewProvider {
@@ -81,8 +135,14 @@ struct ControlPanelView_Previews: PreviewProvider {
 			previewState.connecting = false
 			previewState.fixableConfigs = [
 				FixableId("header") : .divider(display: FixableDisplay("Header", order: 0)),
-				FixableId("slider1") : .float(min: 0.25, max: 1.0, display: FixableDisplay("Slider 1", order: 3)),
-				FixableId("slider2") : .float(min: 0.0, max: 360.55, display: FixableDisplay("Slider 2", order: 2)),
+				FixableId("top-group") : .group(contents: [
+					(FixableId("slider1"), .float(min: 0.25, max: 1.0, display: FixableDisplay("Slider 1", order: 3))),
+					(FixableId("slider2"), .float(min: 0.0, max: 360.55, display: FixableDisplay("Slider 2", order: 2)))
+				], display: FixableDisplay("Top sliders", order: 2)),
+				FixableId("bottom-group") : .group(contents: [
+					(FixableId("slider3"), .float(min: 0.25, max: 1.0, display: FixableDisplay("Slider 3", order: 3))),
+					(FixableId("slider4"), .float(min: 0.0, max: 360.55, display: FixableDisplay("Slider 4", order: 2)))
+				], display: FixableDisplay("Bottom sliders", order: 3)),
 				FixableId("toggle") : .bool(display: FixableDisplay("Toggle", order: 1)),
 				FixableId("color") : .color(display: FixableDisplay("Color", order: 5))
 			]
